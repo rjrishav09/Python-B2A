8d57d0547c54966d54bb79a77be381ef17a2292b
Rishav Bhardwaj <crazyrishavhacker@gmail.com>
2026-08-14 19:52:44 +0530

    add till loops

--- main.py
+++ main.py
@@ -5,8 +5,86 @@ height = 5.9           # float
 is_student = True      # bool (boolean)
 nothing = None         # NoneType
 
+#Check type:
 print(type(name))
 print(type(age))
 print(type(height))
 print(type(is_student))
 print(type(nothing))
+
+#Multiple assignment:
+a, b, c, = "Rishav", 24, 5.9 
+print(a,"\n", b,"\n",c)
+a = b = c = 100
+print(f"Multiple assignement values:\n {a},{b},{c}")
+
+#Basic Operators:
+
+# Arithmetic
+print(10 + 3)    # 13
+print(10 - 3)    # 7
+print(10 * 3)    # 30
+print(10 / 3)    # 3.333... (float division)
+print(10 // 3)   # 3 (floor division) (Remove values after decimals)
+print(10 % 3)    # 1 (modulus) (Gives you Remainder)
+print(10 ** 3)   # 1000 (power)
+
+# Comparison
+print(5 == 5)    # True
+print(5 != 3)    # True
+print(5 > 3)     # True
+
+# Logical
+print(True and False)  # False 
+print(True or False)   # True
+print(not True)        # False
+
+#Strings:
+text = "Hello Python"
+
+print(text[0])          # H
+print(text[-1])         # n
+print(text[0:5])        # Hello (slicing) (Very important for data cleaning)
+print(len(text))        # 12 (also count blank space)
+
+# Useful methods
+print(text.upper())
+print(text.lower())
+print(text.replace("Python", "World"))
+print("Python" in text)  # True
+
+# f-strings (modern way)
+name = "Arzun"
+age = 22
+print(f"My name is {name} and I am {age} years old")
+
+#Control Flow: (if-elif-else)
+age = 18
+
+if age >= 18:
+    print("Adult")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("Child")
+
+#for loop:
+for i in range(5):          # 0 to 4
+    print(i)
+
+for fruit in ["apple", "banana", "mango"]:
+    print(fruit)
+
+#while loop:
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+#break and continue:
+for i in range(10):
+    if i == 3:
+        continue      # skip 3
+    if i == 7:
+        break         # stop at 7
+    print(i)
