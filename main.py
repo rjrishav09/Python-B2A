14447b09389ef03a082e4905614bc76f0fb81f43
Rishav Bhardwaj <crazyrishavhacker@gmail.com>
2026-08-14 19:41:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1. Variables and Data Types
+name = "Alex"          # str (string)
+age = 25               # int (integer)
+height = 5.9           # float
+is_student = True      # bool (boolean)
+nothing = None         # NoneType
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(nothing))
