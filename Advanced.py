9dc3f6fbaa6465354ba3e4d0a6accab36e4efc5f
Rishav Bhardwaj <crazyrishavhacker@gmail.com>
2026-08-18 20:22:47 +0530

    adding files

--- Advanced.py
+++ Advanced.py
@@ -71,7 +71,7 @@ def slow():
 slow()
 
 
-# Class-based Decorator (Class for learning)
+# Class-based Decorator (Class for learning) things are good
 class CountCalls:
     def __init__(self, func):
         self.func = func
